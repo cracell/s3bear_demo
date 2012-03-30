64fdebea222df8f92c25efcb77aa3f2463361d66
Eric Cranston <cracell@gmail.com>
2012-03-30 16:35:27 -0700

    Initial commit of S3 Bear Demo Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S3BearDemo::Application.config.secret_token = '4aa18c288f96dc1f218c9b241e118ca9e5683fc3626785d82e8b84755f1b31acf8abcd866327b9e5d87f3aaa2966cc777c27c0fe9ff9ecf127f4706f6a78df1f'
